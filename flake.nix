ab06801854de55c2279b74e9e93a0f3f5e2237ac
tacascer <trandangtrithanh2000@gmail.com>
2026-04-07 10:36:31 +0100

    feat: add nix flake dev environment, replace mise
    Provide reproducible dev tooling (bazelisk, gcc, gradle, pre-commit)
    via a nix flake. Add NixOS sandbox workaround to .envrc and .bazelrc.
    Remove mise.toml in favor of nix-managed gradle.

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Dev shell for gradle-build-scan-server";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.bazelisk
+              pkgs.gcc
+              pkgs.gradle
+              pkgs.pre-commit
+            ];
+
+            # Linux: set NIX_LD so bazelisk can run downloaded Bazel binaries
+            # On NixOS, this additionally requires `programs.nix-ld.enable = true`
+            env = pkgs.lib.optionalAttrs pkgs.stdenv.isLinux {
+              NIX_LD = pkgs.stdenv.cc.bintools.dynamicLinker;
+              NIX_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+                pkgs.stdenv.cc.cc.lib
+                pkgs.zlib
+              ];
+            };
+          };
+        }
+      );
+    };
+}
